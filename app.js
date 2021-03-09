0f52c459e856cfcd5b4582e414b1b098f16bac38
hkshakib <70447689+hkshakib@users.noreply.github.com>
2021-03-10 05:52:41 +0600

    Create app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Main from './app/component/main'
+import { StyleSheet, Text, View } from 'react-native';
+import { render } from 'react-dom';
+
+export default class App extends React.Component{
+  render(){
+  return (
+    <Main/>
+  );
+  }
+}
+
+
